582df28e2bfbbf91806eb91e984bf5a3ff27f842
ly49nkallo <65131672+ly49nkallo@users.noreply.github.com>
2023-04-01 22:40:44 -0700

    r

--- research.py
+++ research.py
@@ -1,16 +1,22 @@
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 import autograd as a
 import autograd.utils as u
+import autograd.functional as f
 import numpy as np
+import pickle
+import os
+from pathlib import Path
+from mnist_nn import MNIST_MLP # get definition
 '''INVESTIGATE CRITICALITY IN MULTI-LAYER PERCEPTRONS'''
 
-class MLP(a.Module):
+class XOR(a.Module): 
     def __init__(self):
         super().__init__()
-        self.linear1 = a.Linear(100,100)
-        self.linear2 = a.Linear(100,100)
-        self.act1 = a.Sigmoid()
-        self.act2 = a.Sigmoid()
-
+        self.linear1 = a.Linear(2,2)
+        self.linear2 = a.Linear(2,1)
+        self.act1, self.act2 = a.Sigmoid(), a.Sigmoid()
+    
     def forward(self, x):
         x = self.linear1(x)
         x = self.act1(x)
@@ -19,15 +25,29 @@ class MLP(a.Module):
         return x
     
 def main():
-    dataloader = a.Dataloader(dataset='NOT', batch_size=10, train=False)
-    test_dataloader = a.Dataloader(dataset='NOT', batch_size=10, train=True)
-    history = []
-    model = MLP()
-    optim = a.SGD(model.parameters(), lr=0.02)
-    u.serialize_model(model)
-    del model
-    model = u.load_model(name='MLP')
-    print(list(model.named_parameters()))
+    # visualize each activation at each layer'
+    name = 'MNIST_MLP'
+    with open(Path(f"{os.getcwd()}/saved_models/{name}.pkl"), 'rb') as inp:
+        model =  pickle.load(inp)
+    params = list(model.parameters())
+    params = [p.data for p in params] # get just the data
+    init_state = np.zeros(shape=(1,28*28))
+    history = [init_state.copy()]
+    state = init_state
+    for p in params:
+        if p.ndim == 1:
+            state = state + p
+            state = f.stable_softmax(state)
+            history.append(state.copy())
+        elif p.ndim == 2:
+            state = state @ p
+        #record state
+        
+    fig, ax = plt.subplots(1, len(history), sharey=True)
+    for i, a in enumerate(ax.flatten()):
+        a.imshow(history[i])
+    plt.show()
+    print(state)
 
 
 if __name__ == '__main__':
