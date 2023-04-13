cf79041d213e5ce4065a9f813c0f7d10d519d123
ly49nkallo <65131672+ly49nkallo@users.noreply.github.com>
2023-04-12 17:31:11 -0700

    btr

--- research.py
+++ research.py
@@ -4,30 +4,56 @@ import autograd as a
 import autograd.utils as u
 import autograd.functional as f
 import numpy as np
-import matplotlib.pyplot as plt
+import pickle
+import os
+from pathlib import Path
+from mnist_nn import MNIST_MLP, Custom_MNIST_MLP # get definition(s)
 '''INVESTIGATE CRITICALITY IN MULTI-LAYER PERCEPTRONS'''
     
 def main():
-    model = u.load_model(name="MNIST_MLP")
-    parameters = [p.data for p in model.parameters()]
-    input_state = np.zeros(28**2)
-    input_state[28**2//2:] = 1
-    input_state = a.Tensor(input_state.reshape((-1,28**2)))
-    # plt.imshow(input_state.data.reshape((28,28)))
-    print(model(input_state).data)
+    # visualize each activation at each layer'
+    name = 'Custom_MNIST_MLP'
+    with open(Path(f"{os.getcwd()}/saved_models/{name}.pkl"), 'rb') as inp:
+        model =  pickle.load(inp)
+    params = list(model.parameters())
+    params = [p.data for p in params] # get just the data
+    def simulate(init_state):
+        history = [init_state]
+        state = init_state
+        for p in params:
+            if p.ndim == 1:
+                state = state + p
+                state = f.stable_softmax(state)
+                history.append(state.copy())
+            elif p.ndim == 2:
+                state = state @ p
+        return history
+    h1 = simulate(np.zeros(shape=(1,28*28), dtype=np.float32))
+    init_state_2 = np.zeros(shape=(1,28*28), dtype=np.float32)
+    init_state_2[0,0] += 1
+    h2 = simulate(init_state_2)
+    assert len(h1) == len(h2), 'Histories not same length'
+    #record state
+    fig, ax = plt.subplots(2, len(h1))
 
-    '''Test many different pertubations in the network'''
-    state = input_state.data
-    weights = []
-    for p in model.parameters():
-        if p.ndim == 2: weights.append(p.data)
-    fig, axs = plt.subplots(1,len(weights), squeeze=True)
-    for i, ax in enumerate(axs):
-        ax.imshow(weights[i].squeeze())
-    # print(state)
+    '''Visualize state timeseries'''
+    for i, a in enumerate(ax.flatten()[0:len(h1)]):
+        a.axis('off')
+        if i == len(h1) - 1:
+            a.imshow(h1[i])
+            break
+        a.imshow(h1[i].reshape(int(np.sqrt(h1[i].size)),int(np.sqrt(h1[i].size))))
+    ''''''
+    '''Visualize differences'''
+    for i, a in enumerate(ax.flatten()[len(h1):]):
+        a.axis('off')
+        image = h2[i] - h1[i]
+        if i == len(h2) - 1:
+            a.imshow(image)
+            break
+        a.imshow(image.reshape(int(np.sqrt(image.size)),int(np.sqrt(image.size))))
     plt.show()
-
-    
+    ''''''
 
 
 if __name__ == '__main__':
