6daed0529442a32f2b2a33c5b6c67da0078f582c
ly49nkallo <65131672+ly49nkallo@users.noreply.github.com>
2023-04-12 22:24:17 -0700

    yay

--- research.py
+++ research.py
@@ -9,7 +9,73 @@ import os
 from pathlib import Path
 from mnist_nn import MNIST_MLP, Custom_MNIST_MLP # get definition(s)
 '''INVESTIGATE CRITICALITY IN MULTI-LAYER PERCEPTRONS'''
+def display(h1, h2):
+    #record state
+    fig, ax = plt.subplots(4, len(h1))
+
+    '''Visualize state timeseries'''
+    for i, a in enumerate(ax[0,:]):
+        a.set_title(f'State {i}')
+        a.axis('off')
+        if i == len(h1) - 1:
+            a.imshow(h1[i])
+            break
+        a.imshow(h1[i].reshape(int(np.sqrt(h1[i].size)),int(np.sqrt(h1[i].size))))
+    ''''''
+    '''Visualize pertubation timeseries'''
+    for i, a in enumerate(ax[1,:]):
+        a.axis('off')
+        if i == len(h2) - 1:
+            a.imshow(h1[i])
+            break
+        a.imshow(h2[i].reshape(int(np.sqrt(h2[i].size)),int(np.sqrt(h2[i].size))))
+    '''Visualize differences'''
+    diff = [h2[i] - h1[i] for i in range(len(h1))]
+    for i, a in enumerate(ax[2,:]):
+        a.axis('off')
+        image = diff[i]
+        if i == len(h2) - 1:
+            a.imshow(image)
+            break
+        a.imshow(image.reshape(int(np.sqrt(image.size)),int(np.sqrt(image.size))))
+    ''''''
+    '''Significance threshold'''
+    std = [np.std(diff[i]) for i in range(len(diff))]
+    for i, a in enumerate(ax[3,:]):
+        a.axis('off')
+        a.set_title(f"std: {int(std[i] * 1000)/1000}")
+        significance_factor = 1.5
+        image = (diff[i] > std[i] * significance_factor).astype(np.byte)
+        if i == len(h2) - 1:
+            a.imshow(image)
+            break
+        a.imshow(image.reshape(int(np.sqrt(image.size)),int(np.sqrt(image.size))))
+    ''''''
+    plt.show()
+
+def display2(input_states, simulate):
+    h1 = simulate(np.zeros(shape=(1,28*28), dtype=np.float32))
+    fig, ax = plt.subplots(len(input_states), len(h1), squeeze=True)
+    if len(input_states) == 1:
+        ax = ax.reshape(1, -1)
+    for input_idx, input_state in enumerate(input_states):
+        h2 = simulate(input_state)
+        diff = [h2[i] - h1[i] for i in range(len(h1))]
+        '''Significance threshold'''
+        std = [np.std(diff[i]) for i in range(len(diff))]
+        for i, a in enumerate(ax[input_idx,:]):
+            a.axis('off')
+            a.set_title(f"std: {int(std[i] * 1000)/1000}")
+            significance_factor = 1.5
+            image = (diff[i] > std[i] * significance_factor).astype(np.byte)
+            if i == len(h2) - 1:
+                a.imshow(image)
+                break
+            a.imshow(image.reshape(int(np.sqrt(image.size)),int(np.sqrt(image.size))))
+        ''''''
     
+    plt.show()
+
 def main():
     # visualize each activation at each layer'
     name = 'Custom_MNIST_MLP'
@@ -28,33 +94,29 @@ def main():
             elif p.ndim == 2:
                 state = state @ p
         return history
-    h1 = simulate(np.zeros(shape=(1,28*28), dtype=np.float32))
-    init_state_2 = np.zeros(shape=(1,28*28), dtype=np.float32)
-    init_state_2[0,0] += 1
-    h2 = simulate(init_state_2)
-    assert len(h1) == len(h2), 'Histories not same length'
-    #record state
-    fig, ax = plt.subplots(2, len(h1))
+    
+    init_states = []
+    size_of_flash = 5
+    for i in range(3):
+        init_state = np.zeros(shape=(1,28,28), dtype=np.float32)
+        x, y = np.random.randint(0,28-size_of_flash), np.random.randint(0,28-size_of_flash)
+        init_state[0,x:x+size_of_flash,y:y+size_of_flash] += 1
+        init_state = init_state.reshape(1,-1)
+        init_states.append(init_state)
 
-    '''Visualize state timeseries'''
-    for i, a in enumerate(ax.flatten()[0:len(h1)]):
-        a.axis('off')
-        if i == len(h1) - 1:
-            a.imshow(h1[i])
-            break
-        a.imshow(h1[i].reshape(int(np.sqrt(h1[i].size)),int(np.sqrt(h1[i].size))))
-    ''''''
-    '''Visualize differences'''
-    for i, a in enumerate(ax.flatten()[len(h1):]):
-        a.axis('off')
-        image = h2[i] - h1[i]
-        if i == len(h2) - 1:
-            a.imshow(image)
-            break
-        a.imshow(image.reshape(int(np.sqrt(image.size)),int(np.sqrt(image.size))))
-    plt.show()
-    ''''''
+    display2(init_states, simulate)
 
-
+def config():
+    font = {'family' : 'DejaVu Sans',
+            'weight' : 'bold',
+            'size'   : 8}
+    import matplotlib
+    matplotlib.rc('font', **font)
+    cmaps = ['binary', 'gist_yarg', 'gist_gray', 'gray', 'bone',
+             'pink', 'spring', 'summer', 'autumn', 'winter', 'cool',
+             'Wistia', 'hot', 'afmhot', 'gist_heat', 'copper']
+    sequential = ['viridis', 'plasma', 'inferno', 'magma', 'cividis']
+    matplotlib.rc('image', cmap="gray")
 if __name__ == '__main__':
+    config()
     main()
